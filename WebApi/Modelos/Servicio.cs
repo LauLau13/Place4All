874e1e108863caeea7cc24ea1fff458485f7fc55
Laura Gil <a25589svalero.com>
2022-11-16 19:54:30 +0100

    Corrección campos no nulables

--- WebApi/Modelos/Servicio.cs
+++ WebApi/Modelos/Servicio.cs
@@ -5,8 +5,8 @@ namespace WebApi.Modelos
     public class Servicio
     {
         [Required(ErrorMessage = "El ID del Servicio es obligatorio")]
-        private int? ID { get; set; }
-        private string? Nombre { get; set; }
+        private int ID { get; set; }
+        private string Nombre { get; set; } = "";
         private string Descripcion { get; set; }
         
 
