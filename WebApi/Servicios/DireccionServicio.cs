3dfd40386d8fe08019974cff009a2b6edefc8b4a
Laura Gil <a25589svalero.com>
2022-11-16 21:04:42 +0100

    Generado métodos

--- WebApi/Servicios/DireccionServicio.cs
+++ WebApi/Servicios/DireccionServicio.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApi.Servicios
+﻿using WebApi.Modelos;
+using MongoDB.Driver;
+
+namespace WebApi.Servicios
 {
     public class DireccionServicio
     {
+        private readonly IMongoCollection<Direccion> _direcciones;
+        
+        public DireccionServicio(IDireccionesDataBaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DataBaseName);
+
+            _direcciones = database.GetCollection<Direccion>(settings.DireccionesCollectionName);
+        }
+
+        public List<Direccion> Get() => _direcciones.Find(direccion => true).ToList();
+
+        public Direccion Get(int id) => _direcciones.Find(direccion => direccion.ID == id);
     }
 }
