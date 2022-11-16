cb0efef70659a42706a7ba2ee4eac7f80a0d72fc
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-16 19:18:36 +0100

    Añadido modelo de Dirección

---
+++ WebApi/Modelos/Direccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Modelos
+{
+    public class Direccion
+    {
+        
+        private int ID { get; set; }
+        [Required]
+        private string? Calle { get; set; }
+        private int Numero { get; set; }
+        private string? Ciudad { get; set; }
+        private string? CP { get; set; }
+        private string? Provincia { get; set; }
+    }
+}
