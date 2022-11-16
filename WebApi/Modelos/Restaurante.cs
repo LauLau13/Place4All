c6aa90186c9c6f5611cccbde5e73c2df59a640f0
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-16 22:40:32 +0100

    Se abren los atributos a publico para ser usado en otros métodos.

--- WebApi/Modelos/Restaurante.cs
+++ WebApi/Modelos/Restaurante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,13 @@ namespace WebApi.Modelos
     public class Restaurante
     {
         [Required (ErrorMessage = "El id de dirección es obligatorio")]
-        private int ID { get; set; }
-        private string Nombre { get; set; } = "";
-        private Direccion Direccion { get; set; } //Tabla de direcciones
-        private string Descripcion { get; set; } = "";
-        private string Telefono { get; set; } = "";
-        private string Imagen { get; set; } = "";
-        private list<Servicio> Servicio { get; set; }
+        public int ID { get; set; }
+        public string Nombre { get; set; } = "";
+        public Direccion Direccion { get; set; }
+        public string Descripcion { get; set; } = "";
+        public string Telefono { get; set; } = "";
+        public string Imagen { get; set; } = "";
+        public List<Servicio> Servicio { get; set; } = new List<Servicio>();
 
     }
 }
