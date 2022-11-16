01ae58d78832a68deb600d1eea9bcf6454787cbd
Laura Gil <a25589svalero.com>
2022-11-16 19:36:48 +0100

    Creada la clase Modelo Servicios

---
+++ WebApi/Modelos/Servicio.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Modelos
+{
+    public class Servicio
+    {
+        //Atributos
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+        static int nextId; //para el ID autoincremental
+        private int ID { get; set; }
+
+        //Constructor
+        public Servicio(string nombre, string descripcion)
+        {
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.ID = Interlocked.Increment(ref nextId);
+
+        }
+
+    }
+}
