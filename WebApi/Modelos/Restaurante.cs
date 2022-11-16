9e14d4805719e5d7faaef0e0190846a8192a5652
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-11-16 18:48:09 +0100

    añadido modelo de restaurante

---
+++ WebApi/Modelos/Restaurante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Modelos
+{
+    public class Restaurante
+    {
+        private int ID { get; set; }
+        [Required]
+        private string Nombre { get; set; }
+        private Direccion Direccion { get; set; }//Tabla de direcciones
+        private string Descripcion { get; set; }
+        private string Telefono { get; set; }
+        private string Imagen { get; set; }
+        private list<Servicio> Servicio { get; set; }
+
+    }
+}
