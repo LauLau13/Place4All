44dcb569848ce8ad43684c906c9866e6b639e16c
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-20 12:22:48 +0100

    Configuracion inicial forntal

--- FrontProyectoES/src/app/app.module.ts
+++ FrontProyectoES/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ FrontProyectoES/src/app/restaurante/restaurante.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RestauranteRoutingModule } from './restaurante-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RestauranteRoutingModule],
+})
+export class RestauranteModule {}
