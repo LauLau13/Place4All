b7a347fc179f28e1ecbaa626d71134719da973a1
Laura Gil <a25589svalero.com>
2022-11-23 20:04:55 +0100

    Comentarios en DatabaseSettings y en launchSettings

--- WebApi/Modelos/DatabaseSettings.cs
+++ WebApi/Modelos/DatabaseSettings.cs
@@ -1,5 +1,6 @@
 namespace WebApi.Modelos
 {
+    //Clase creada para la configuración de la base de datos
     public class DatabaseSettings : IDatabaseSettings
     {
         public string CollectionName { get; set; }
@@ -7,6 +8,7 @@ namespace WebApi.Modelos
         public string DatabaseName { get; set; }
     }
 
+    //Interfaz creada para la clase DatabaseSettings
     public interface IDatabaseSettings
     {
         string CollectionName { get; set; }
