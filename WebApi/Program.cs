832fea390864c93acdb6d4acb3d63a7de2aa263f
Laura Gil <a25589svalero.com>
2022-11-23 19:03:48 +0100

    Corregido BUG

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using WebApi.Modelos;
+using WebApi.Servicios;
 
 namespace WebApi
 {
@@ -7,15 +12,64 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            //CreateHostBuilder(args).Build().Run();//Eliminar
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+            //Creación del contenedor de la aplicación llamado builder
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Configuración de la base de datos en el contenedor builder
+            builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
+
+            //Para evitar errores en la ejecución de la API
+            builder.Services.AddMvc(options =>
             {
-                webBuilder.UseStartup<Startup>();
+                //Método que cuando es true recorta el sufijo Async aplicado en los métodos
+                options.SuppressAsyncSuffixInActionNames = false;
             });
+
+            //Permite el acceso de los servicios a la base de datos
+            builder.Services.AddSingleton<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+            //Añadir cada servicio de la siguiente manera: services.AddSingleton<{Nombre del servicio}>();
+            builder.Services.AddSingleton<ServicioServicio>();
+            builder.Services.AddSingleton<DireccionServicio>();
+            builder.Services.AddSingleton<UsuarioServicio>();
+
+            //Añade los controladores de los servicios
+            builder.Services.AddControllers();
+
+            //Añade un documento swagger para controlar la API
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
+
+            //Tras realizar los pasos anteriores se ejecuta el builder
+            var app = builder.Build();
+            
+            //Configuración de las peticiones con HTTP
+            if (builder.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.Run();
+        }
+        
+        
     }
 }
 
