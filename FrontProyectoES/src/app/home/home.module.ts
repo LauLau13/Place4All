a63fec1b380895714251f434b2686516c872df4f
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-26 12:28:41 +0100

    Maquetación incial landing

--- FrontProyectoES/src/app/home/home.module.ts
+++ FrontProyectoES/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
