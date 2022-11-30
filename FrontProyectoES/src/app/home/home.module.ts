84c415391f85b1fc5d237d595a3ab0b502489970
Alexander Aguirre <keevinaguirre@gmail.com>
2022-11-30 18:11:42 +0100

    Ajustes login

--- FrontProyectoES/src/app/home/home.module.ts
+++ FrontProyectoES/src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, NgbAlertModule],
 })
 export class HomeModule {}
