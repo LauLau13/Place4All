848c7f850ee81dd49a5508f59e526638a9f98f47
Alexander Aguirre <keevinaguirre@gmail.com>
2022-12-06 15:01:06 +0100

    Formulario para login

--- FrontProyectoES/src/app/home/home.module.ts
+++ FrontProyectoES/src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeRoutingModule } from './home-routing.module';
@@ -8,6 +9,6 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, NgbAlertModule],
+  imports: [CommonModule, HomeRoutingModule, NgbAlertModule, ReactiveFormsModule],
 })
 export class HomeModule {}
--- FrontProyectoES/src/app/home/login/login.component.ts
+++ FrontProyectoES/src/app/home/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Login } from 'src/app/shared/Models/login.model';
+import { LoginService } from 'src/app/shared/services/LoginService/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private loginService: LoginService) {}
   error = false;
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
   ngOnInit(): void {}
+
+  async login() {
+    var login: Login = {
+      email: this.loginForm.value.email!,
+      password: this.loginForm.value.password!,
+    };
+
+    console.log(login);
+    debugger;
+    const res = await this.loginService.login(login);
+    if (!res) {
+      this.error = true;
+    }
+  }
 }
