37b524dde0fa7434bf360eda9132184f484a596e
Alexander Aguirre <keevinaguirre@gmail.com>
2022-12-06 15:24:39 +0100

    Se incorpora el componente de registro

--- FrontProyectoES/src/app/home/home.module.ts
+++ FrontProyectoES/src/app/home/home.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, HomeRoutingModule, NgbAlertModule],
 })
 export class HomeModule {}
