06551178958ac6101dc62820d0413e8203edb14e
Laura Gil <a25589svalero.com>
2022-12-07 18:38:09 +0100

    Generado el componente listado de restaurantes

--- FrontProyectoES/src/app/restaurante/restaurante.module.ts
+++ FrontProyectoES/src/app/restaurante/restaurante.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RestauranteRoutingModule } from './restaurante-routing.module';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ListadoComponent],
   imports: [CommonModule, RestauranteRoutingModule],
 })
 export class RestauranteModule {}
