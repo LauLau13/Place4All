5de4387c7210b0a4d7f3a21df2c47977f4d90be4
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-12-07 19:45:25 +0100

    restaurante

--- FrontProyectoES/src/app/app-routing.module.ts
+++ FrontProyectoES/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+//TODO: BORRAR RUTA
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    /* loadChildren: () => import('./home/home.module').then(m => m.HomeModule), */
+
+    loadChildren: () => import('./restaurante/restaurante.module').then(m => m.RestauranteModule),
   },
   {
     path: '**',
     redirectTo: '',
     pathMatch: 'full',
+  },
+  {
+    path: 'restaurante',
+    loadChildren: () => import('./restaurante/restaurante.module').then(m => m.RestauranteModule),
   },
 ];
 
--- FrontProyectoES/src/app/restaurante/restaurante.module.ts
+++ FrontProyectoES/src/app/restaurante/restaurante.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { DetalleComponent } from './detalle/detalle.component';
 import { RestauranteRoutingModule } from './restaurante-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [DetalleComponent],
   imports: [CommonModule, RestauranteRoutingModule],
 })
 export class RestauranteModule {}
