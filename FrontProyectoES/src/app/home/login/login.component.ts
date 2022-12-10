f61e647530dc940d5f672161c5d3d24822755b23
Alexander Aguirre <keevinaguirre@gmail.com>
2022-12-10 23:27:43 +0100

    Modelos

--- FrontProyectoES/src/app/home/login/login.component.ts
+++ FrontProyectoES/src/app/home/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Login } from 'src/app/shared/Models/login.model';
+import { Login, LoginResponse } from 'src/app/shared/Models/login.model';
 import { LoginService } from 'src/app/shared/services/LoginService/login.service';
 import { RegisterComponent } from '../register/register.component';
 
@@ -24,13 +24,15 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.value.email!,
       password: this.loginForm.value.password!,
     };
-
     console.log(login);
+
+    let res: LoginResponse = await this.loginService.login(login);
     debugger;
-    const res = await this.loginService.login(login);
     if (!res) {
       this.error = true;
     }
+    this.loginService.setToken(res.token);
+    this.loginService.setUser(res.usuario);
   }
   openRegister() {
     this.modalService.dismissAll();
--- FrontProyectoES/src/app/shared/services/LoginService/login.service.ts
+++ FrontProyectoES/src/app/shared/services/LoginService/login.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Login } from '../../Models/login.model';
+import { Login, LoginResponse } from '../../Models/login.model';
+import { Usuario } from '../../Models/usuario.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,23 +11,28 @@ export class LoginService {
   api = environment.api;
   constructor(private http: HttpClient) {}
 
-  async login(body: Login): Promise<boolean> {
-    fetch('https://localhost:7157/usuario/login', {
-      method: 'POST',
-      headers: {},
-      body: JSON.stringify(body),
-    })
-      .then(response => {
-        localStorage.setItem('token', JSON.stringify(response));
-        return true;
-      })
-      .catch(err => {
-        console.log(err);
-        return false;
-      });
+  async login(body: Login): Promise<LoginResponse> {
+    /* return this.http.post<LoginResponse>(`${this.api}/Usuario/login`, JSON.stringify(body)); */
 
-    return false;
+    const response = await fetch(`${this.api}/Usuario/login`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+    const result = await response.json();
+    return result;
   }
 
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  setUser(usuario: Usuario) {
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+  }
   logOut() {}
 }
