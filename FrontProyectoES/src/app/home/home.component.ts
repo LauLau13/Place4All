3b1f455e982aaf2c0ef382582c963cb0039465d1
Alexander Aguirre <keevinaguirre@gmail.com>
2022-12-11 13:58:45 +0100

    Cambio de labels

--- FrontProyectoES/src/app/home/home.component.ts
+++ FrontProyectoES/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private restauranteService: RestauranteService) {}
+  constructor(private router: Router) {}
   searchField: string = '';
   ngOnInit(): void {}
-  onSearch() {}
+  onSearch() {
+    this.router.navigate(['/restaurantes', { ciuidad: this.searchField }]);
+  }
 }
